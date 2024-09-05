f717827eb54fa9329a98753290c0ae9b1ebf031f
Daniel Iyiola <iyiola_daniel@yahoo.com>
2024-09-05 14:55:03 +0100

    load script and queries have been updated

--- git-linux/assignment/parch_and_posey/psql_load.sh
+++ git-linux/assignment/parch_and_posey/psql_load.sh
@@ -6,6 +6,14 @@ set -e
 # Load environment variables from .env file
 source dbaccess.env
 
+# Check if CSV files exist
+for file in "$DATA_FILES"/*.csv; do
+    if [ ! -f "$file" ]; then
+        echo "File $file does not exist. Please check the path."
+        exit 1
+    fi
+done
+
 # Function to load CSV files into PostgreSQL tables
 load_csv_files() {
     echo "Loading CSV files into $DB_NAME database..."
@@ -13,7 +21,7 @@ load_csv_files() {
         table_name=$(basename "$csv_file" .csv)  # Get table name from the CSV file name
         echo "Loading $csv_file into $table_name table..."
         
-        sudo -u postgres psql -d "$DB_NAME" -c "\COPY $table_name FROM '$csv_file' WITH CSV HEADER;"
+        sudo -u postgres psql -d "$DB_NAME" -c "\COPY $table_name FROM '$csv_file' WITH (FORMAT csv, HEADER true);"
     done
 }
 
---
+++ git-linux/assignment/parch_and_posey/queries.sql
@@ -0,0 +1,18 @@
+/* Find a list of order IDs where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table. */
+SELECT id FROM orders
+WHERE poster_qty > 4000;
+
+/* Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000. */
+SELECT * FROM orders
+WHERE standard_qty = 0 AND (gloss_qty > 1000 or poster_qty > 1000);
+
+/* Find all the company names that start with a 'C' or 'W', and where the primary contact contains 'ana' or 'Ana', but does not contain 'eana'. */
+SELECT * FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+	AND (primary_poc LIKE '%Ana%' OR primary_poc LIKE '%ana%')
+	AND primary_poc NOT LIKE '%eana%';
+
+/* Provide a table that shows the region for each sales rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) by account name. */
+SELECT r.name, sr.name, a.name FROM sales_reps sr
+	JOIN region r ON sr.region_id = r.id
+	JOIN accounts a ON sr.id = a.sales_rep_id;

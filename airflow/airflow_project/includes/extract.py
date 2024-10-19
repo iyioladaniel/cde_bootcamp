735e714154dabd835508ed34328126038abef9bd
Daniel Iyiola <iyiola_daniel@yahoo.com>
2024-10-19 23:40:57 +0100

    updated extract.py

---
+++ airflow/airflow_project/includes/extract.py
@@ -0,0 +1,9 @@
+
+def _fetch_pageviews(pagenames):
+    result = dict.fromkeys(pagenames, 0)
+    with open(f"/tmp/wikipageviews", "r") as f:
+        for line in f:
+            domain_code, page_title, view_counts, _ = line.split(" ")
+            if domain_code == "en" and page_title in pagenames:
+                result[page_title] = view_counts
+    print(result)
